77ddd07d0097529bbbe6844eafd7ffb31f3ec902
Oliver Diamond <diamond.oliver@gmail.com>
2020-02-11 15:24:59 +0000

    debug

--- clone.sh
+++ clone.sh
@@ -1,15 +1,20 @@
-ENV_REPO=$(kubectl get env this -o jsonpath='{.spec.source.url}')
-echo ENV_REPO: $ENV_REPO
+set -x
+# ENV_REPO=$(kubectl get env this -o jsonpath='{.spec.source.url}')
+# echo ENV_REPO: $ENV_REPO
 
 GIT_URL=$(echo $ENV_REPO | sed -e "s/^https:\/\//https:\/\/$GITHUB_ACCESS_TOKEN@/")
 echo GIT_URL: $GIT_URL
 
 git clone $GIT_URL deployment
+ls -latr
 
 cd deployment/env
+ls -latr
 
-jx step helm apply
+JX_LOG_LEVEL=debug jx step helm apply
 
 cd ../../
+ls -latr
 
 rm -rf deployment
+ls -latr
