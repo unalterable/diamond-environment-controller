746a2977597f5a32b5ba6e5aff87456677028e0d
Oliver Diamond <diamond.oliver@gmail.com>
2020-02-25 12:57:59 +0000

    bugfix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const childProcess = require('child_process')
 const port = 3000
 
-console.log = (...args) => console.log((new Date()).toISOString(), ...args)
+const log = (...args) => console.log((new Date()).toISOString(), ...args)
 
 const addToPromiseChain = ((chain, iteration) => newItem => {
   const thisIteration = iteration++
@@ -11,9 +11,9 @@ const addToPromiseChain = ((chain, iteration) => newItem => {
 })(Promise.resolve(), 1)
 
 const exec = (cmd, pre) => new Promise((resolve, reject) => {
-  console.log(pre, cmd)
+  log(pre, cmd)
   const process = childProcess.exec(cmd, (error, stdout) => error === null ? resolve(stdout) : reject(error.message))
-  process.stdout.on('data', (data) => console.log(pre, data.toString()))
+  process.stdout.on('data', (data) => log(pre, data.toString()))
 })
 
 const processBody = request => new Promise((resolve, reject) => {
@@ -31,15 +31,15 @@ const processBody = request => new Promise((resolve, reject) => {
 const task = async currIteration => {
   const pre = `>>> Iteration ${currIteration}`
   try {
-    console.log(pre, 'starting')
+    log(pre, 'starting')
     const githubUrl = process.env.ENV_REPO.replace('https://', `https://${process.env.GITHUB_ACCESS_TOKEN}@`)
     await exec(`git clone ${githubUrl} deployment`, pre)
     await exec('sh deployment/deploy.sh', pre)
     await exec('rm -rf deployment', pre)
-    console.log(pre, `>>> Iteration ${currIteration} succeeded:`)
+    log(pre, `>>> Iteration ${currIteration} succeeded:`)
   } catch (e) {
-    console.log(`>>> Iteration ${currIteration} failed:`)
-    console.log(e)
+    log(`>>> Iteration ${currIteration} failed:`)
+    log(e)
     await exec('rm -rf deployment && true', pre)
   }
 }
@@ -48,14 +48,14 @@ const requestHandler = async (request, response) => {
   const body = await processBody(request)
   if (request.url === '/github-webhook' && body.ref === "refs/heads/master"){
     const numberInQueue = addToPromiseChain(task)
-    console.log(`${(new Date()).toISOString()} >>> New task registered (Iteration ${numberInQueue})`)
+    log(`>>> New task registered (Iteration ${numberInQueue})`)
     response.end(`OK - New task registered (Iteration ${numberInQueue})`)
   } else {
-    console.log(`${(new Date()).toISOString()} >>> Request Rejected`)
+    log('>>> Request Rejected')
     response.end(`OK`)
   }
 }
 
 http
   .createServer(requestHandler)
-  .listen(port, () => console.log(`server is listening on ${port}`))
+  .listen(port, () => log(`server is listening on ${port}`))
